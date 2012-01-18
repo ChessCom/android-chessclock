2bb255f6d7b1862e8fc92acd259aab13950f4c3a
pdachtera <piotr@dachtera.org>
2012-01-18 10:39:26 +0100

    ##REVERT## Commit:c972a5d0042c57395d7fdd39a69776a8b03a3890
    * Add HomeListItem.class
    
    ##REVERT## Commit:4d995bfe7c3574f4c8b8022aa220b4fd923bcc77
    
    * Sign In Screen Design changes
    Home Screen design changes
    Preferences Screen minor changes
    
    Partly removed hardcoded strings and values from xml layouts
    
    Graphics changed
    App Themes created
    Styles changed
    Dimensional and screen sizes graphics and values added
    
    Home Activity Tab changed SignIn Activity changed
    Game class changed (settings string changed)
    Facebook Login Button changed
    
    ##REVERT## Commit:396d20ef6257d976d4fa27decb266d1bf1507c34
    
    * Change Home screen landscape and portrait layout
    Add dimens params to widgets

--- src/main/java/com/chess/core/Tabs.java
+++ src/main/java/com/chess/core/Tabs.java
@@ -1,20 +1,9 @@
 package com.chess.core;
 
-import android.app.TabActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Typeface;
-import android.net.Uri;
-import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.RelativeLayout;
-import android.widget.TabHost;
-import android.widget.TabWidget;
-import android.widget.TextView;
 import com.chess.R;
 import com.chess.activities.Game;
 import com.chess.activities.Register;
@@ -27,110 +16,138 @@ import com.chess.utilities.MobclixHelper;
 import com.chess.utilities.Notifications;
 import com.mobclix.android.sdk.Mobclix;
 
+import android.app.TabActivity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.RelativeLayout;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
+
 public class Tabs extends TabActivity {
 
 	public MainApp App;
 	private TextView removeAds;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Mobclix.onCreate(this);
-		App = (MainApp) getApplication();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mobclix.onCreate(this);
+        App = (MainApp)getApplication();
 
-		//get global Shared Preferences
-		if (App.sharedData == null) {
-			App.sharedData = getSharedPreferences("sharedData", 0);
-			App.SDeditor = App.sharedData.edit();
-		}
+        //get global Shared Preferences
+        if (App.sharedData == null)
+        {
+          App.sharedData = getSharedPreferences("sharedData", 0);
+          App.SDeditor = App.sharedData.edit();
+        }
 
-		setContentView(R.layout.tabs);
+        setContentView(R.layout.tabs);
 
-		removeAds = (TextView) findViewById(R.id.removeAds);
-		removeAds.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(
-						"http://www." + LccHolder.HOST + "/login.html?als=" + App.sharedData.getString("user_token", "") +
-								"&goto=http%3A%2F%2Fwww." + LccHolder.HOST + "%2Fmembership.html?c=androidads")));
-			}
-		});
+        removeAds = (TextView) findViewById(R.id.removeAds);
+        removeAds.setOnClickListener(new OnClickListener()
+        {
+          public void onClick(View v)
+          {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(
+              "http://www." + LccHolder.HOST + "/login.html?als=" + App.sharedData.getString("user_token", "") +
+              "&goto=http%3A%2F%2Fwww." + LccHolder.HOST + "%2Fmembership.html?c=androidads")));
+          }
+        });
+        
+        if (MobclixHelper.isShowAds(App))
+        {
+          if (MobclixHelper.getBannerAdviewWrapper(App) == null || MobclixHelper.getBannerAdview(App) == null)
+          {
+            MobclixHelper.initializeBannerAdView(this, App);
+          }
+        }
 
-		if (MobclixHelper.isShowAds(App)) {
-			if (MobclixHelper.getBannerAdviewWrapper(App) == null || MobclixHelper.getBannerAdview(App) == null) {
-				MobclixHelper.initializeBannerAdView(this, App);
-			}
-		}
+	    App.mTabHost = getTabHost();
+	    App.mTabHost.addTab(App.mTabHost.newTabSpec("tab1")
+	    		.setIndicator("Home", getResources().getDrawable(R.drawable.home))
+	    		.setContent(new Intent(this, Home.class)));
+	    if(App.guest)
+      {
+	    	App.mTabHost.addTab(App.mTabHost.newTabSpec("tab2")
+          .setIndicator("Live", getResources().getDrawable(R.drawable.live))
+          .setContent(new Intent(this, Register.class).putExtra("liveChess", true)));
+        App.mTabHost.addTab(App.mTabHost.newTabSpec("tab6")
+          .setIndicator("Online", getResources().getDrawable(R.drawable.online))
+          .setContent(new Intent(this, Register.class).putExtra("liveChess", false)));
+      }
+	    else
+      {
+        App.mTabHost.addTab(App.mTabHost.newTabSpec("tab2")
+          .setIndicator("Live", getResources().getDrawable(R.drawable.live))
+          .setContent(new Intent(this, Online.class).putExtra("liveChess", true)));
+        App.mTabHost.addTab(App.mTabHost.newTabSpec("tab6")
+          .setIndicator("Online", getResources().getDrawable(R.drawable.online))
+          .setContent(new Intent(this, Online.class).putExtra("liveChess", false)));
+      }
 
-		App.mTabHost = getTabHost();
-		App.mTabHost.addTab(App.mTabHost.newTabSpec("tab1")
-				.setIndicator("Home", getResources().getDrawable(R.drawable.home))
-				.setContent(new Intent(this, Home.class)));
-		if (App.guest) {
-			App.mTabHost.addTab(App.mTabHost.newTabSpec("tab2")
-					.setIndicator("Live", getResources().getDrawable(R.drawable.live))
-					.setContent(new Intent(this, Register.class).putExtra("liveChess", true)));
-			App.mTabHost.addTab(App.mTabHost.newTabSpec("tab6")
-					.setIndicator("Online", getResources().getDrawable(R.drawable.online))
-					.setContent(new Intent(this, Register.class).putExtra("liveChess", false)));
-		} else {
-			App.mTabHost.addTab(App.mTabHost.newTabSpec("tab2")
-					.setIndicator("Live", getResources().getDrawable(R.drawable.live))
-					.setContent(new Intent(this, Online.class).putExtra("liveChess", true)));
-			App.mTabHost.addTab(App.mTabHost.newTabSpec("tab6")
-					.setIndicator("Online", getResources().getDrawable(R.drawable.online))
-					.setContent(new Intent(this, Online.class).putExtra("liveChess", false)));
-		}
+	    App.mTabHost.addTab(App.mTabHost.newTabSpec("tab3")
+	    		.setIndicator("Comp", getResources().getDrawable(R.drawable.computer))
+	    		.setContent(new Intent(this, Computer.class)));
+	    App.mTabHost.addTab(App.mTabHost.newTabSpec("tab4")
+	    		.setIndicator("Tactics", getResources().getDrawable(R.drawable.tactics))
+	    		.setContent(new Intent(this, Game.class).putExtra("mode", 6).putExtra("liveChess", false)));
+	    App.mTabHost.addTab(App.mTabHost.newTabSpec("tab5")
+	    		.setIndicator("Video", getResources().getDrawable(R.drawable.video))
+	    		.setContent(new Intent(this, Video.class)));
 
-		App.mTabHost.addTab(App.mTabHost.newTabSpec("tab3")
-				.setIndicator("Comp", getResources().getDrawable(R.drawable.computer))
-				.setContent(new Intent(this, Computer.class)));
-		App.mTabHost.addTab(App.mTabHost.newTabSpec("tab4")
-				.setIndicator("Tactics", getResources().getDrawable(R.drawable.tactics))
-				.setContent(new Intent(this, Game.class).putExtra("mode", 6).putExtra("liveChess", false)));
-		App.mTabHost.addTab(App.mTabHost.newTabSpec("tab5")
-				.setIndicator("Video", getResources().getDrawable(R.drawable.video))
-				.setContent(new Intent(this, Video.class)));
+	    TabWidget tw = getTabWidget();
+	    for (int i=0; i<tw.getChildCount(); i++) {
+	        RelativeLayout relLayout = (RelativeLayout)tw.getChildAt(i);
+	        TextView tv = (TextView)relLayout.getChildAt(1);
+	        tv.setTextSize(12);
+	        tv.setTypeface(Typeface.DEFAULT_BOLD);
+	    }
 
-		TabWidget tw = getTabWidget();
-		for (int i = 0; i < tw.getChildCount(); i++) {
-			RelativeLayout relLayout = (RelativeLayout) tw.getChildAt(i);
-			TextView tv = (TextView) relLayout.getChildAt(1);
-			tv.setTextSize(12);
-			tv.setTypeface(Typeface.DEFAULT_BOLD);
-		}
-
-		int tab = 0;
-		try {
-			Bundle extras = getIntent().getExtras();
-			if (extras != null) {
-				if (extras.getBoolean("fromnotif")) {
-					tab = 2;
-					Notifications.resetCounter();
-					if (App.SDeditor != null) {
-						App.SDeditor.putInt("gamestype", 1);
-						App.SDeditor.commit();
-					}
-				} else {
-					tab = extras.getInt("tab", 0);
-					if (tab != 0 && App.SDeditor != null) {
-						App.SDeditor.putInt("gamestype", 1);
-						App.SDeditor.commit();
-					}
-				}
-			}
-		} catch (Exception e) {
+	    int tab = 0;
+	    try{
+		    Bundle extras = getIntent().getExtras();
+		    if(extras != null){
+          if (extras.getBoolean("fromnotif"))
+          {
+            tab = 2;
+            Notifications.resetCounter();
+            if(App.SDeditor != null){
+              App.SDeditor.putInt("gamestype", 1);
+              App.SDeditor.commit();
+            }
+          }
+          else
+          {
+            tab = extras.getInt("tab", 0);
+            if(tab != 0 && App.SDeditor != null){
+              App.SDeditor.putInt("gamestype", 1);
+              App.SDeditor.commit();
+            }
+          }
+		    }
+	    } catch (Exception e) {
 			tab = 0;
 		}
 
-		getTabHost().setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+	    getTabHost().setOnTabChangedListener(new TabHost.OnTabChangeListener() {
 			@Override
-			public void onTabChanged(String tabId) {
+			public void onTabChanged(String tabId)
+			{
 				//System.out.println("LCCLOG2: ONTABCHANGED");
-				if (MobclixHelper.isShowAds(App)) {
-					if (tabId.equals("tab1") || tabId.equals("tab2") || tabId.equals("tab3") || tabId.equals("tab5") || tabId.equals("tab6")) {
+				if (MobclixHelper.isShowAds(App))
+				{
+					if(tabId.equals("tab1") || tabId.equals("tab2") || tabId.equals("tab3") || tabId.equals("tab5") || tabId.equals("tab6"))
+					{
 						//System.out.println("LCCLOG2: ONTABCHANGED 1");
 						MobclixHelper.showBannerAd(MobclixHelper.getBannerAdviewWrapper(App), removeAds, Tabs.this, App);
-					} else if (tabId.equals("tab4")) {
+					}
+					else if (tabId.equals("tab4"))
+					{
 						//System.out.println("LCCLOG2: ONTABCHANGED 2");
 						MobclixHelper.hideBannerAd(App, removeAds);
 					}
@@ -140,61 +157,70 @@ public class Tabs extends TabActivity {
 			}
 		});
 
-		App.mTabHost.setCurrentTab(tab);
-	}
-
-	@Override
-	protected void onResume() {
+	    App.mTabHost.setCurrentTab(tab);
+    }
+	
+    @Override
+    protected void onResume() {
 		super.onResume();
 		//System.out.println("LCCLOG2: TABS ONRESUME");
-		if (MobclixHelper.isShowAds(App)) {
-			//System.out.println("LCCLOG2: TABS ONRESUME 1");
-			final String currentTab = getTabHost().getCurrentTabTag();
-			if (currentTab.equals("tab1") || currentTab.equals("tab2") || currentTab.equals("tab3") || currentTab.equals("tab5") || currentTab.equals("tab6")) {
+      if (MobclixHelper.isShowAds(App))
+      {
+		  //System.out.println("LCCLOG2: TABS ONRESUME 1");
+    	  final String currentTab = getTabHost().getCurrentTabTag();
+    	  if(currentTab.equals("tab1") || currentTab.equals("tab2") || currentTab.equals("tab3") || currentTab.equals("tab5") || currentTab.equals("tab6"))
+			{
 				//System.out.println("LCCLOG2: TABS ONRESUME 2");
 				MobclixHelper.showBannerAd(MobclixHelper.getBannerAdviewWrapper(App), removeAds, this, App);
-			} else if (currentTab.equals("tab4")) {
+			}
+			else if (currentTab.equals("tab4"))
+			{
 				//System.out.println("LCCLOG2: TABS ONRESUME 3");
 				MobclixHelper.hideBannerAd(App, removeAds);
 			}
-			//System.out.println("LCCLOG2: TABS ONRESUME 4");
-		}
-		//System.out.println("LCCLOG2: TABS ONRESUME 5");
-		registerReceiver(lccLoggingInInfoReceiver, new IntentFilter("com.chess.lcc.android-logging-in-info"));
-	}
+		  //System.out.println("LCCLOG2: TABS ONRESUME 4");
+      }
+	  //System.out.println("LCCLOG2: TABS ONRESUME 5");
+	  registerReceiver(lccLoggingInInfoReceiver, new IntentFilter("com.chess.lcc.android-logging-in-info"));
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		if (MobclixHelper.isShowAds(App)) {
-			MobclixHelper.pauseAdview(MobclixHelper.getBannerAdview(App), App);
-		}
-		App.setForceBannerAdOnFailedLoad(false);
-		unregisterReceiver(lccLoggingInInfoReceiver);
-	}
+    @Override
+    protected void onPause()
+	{
+	  super.onPause();
+      if (MobclixHelper.isShowAds(App))
+	  {
+        MobclixHelper.pauseAdview(MobclixHelper.getBannerAdview(App), App);
+      }
+      App.setForceBannerAdOnFailedLoad(false);
+      unregisterReceiver(lccLoggingInInfoReceiver);
+    }
 
-	@Override
-	public void onWindowFocusChanged(boolean hasFocus) {
-		super.onWindowFocusChanged(hasFocus);
-		//System.out.println("LCCLOG2: TABS FOCUS 1");
-		System.out.println("LCCLOG MOBCLIX: onWindowFocusChanged hasFocus=" + hasFocus + ", isForceBannerAdOnFailedLoad=" + App.isForceBannerAdOnFailedLoad());
-		if (MobclixHelper.isShowAds(App) && hasFocus && App.isForceBannerAdOnFailedLoad()) {
-			//System.out.println("LCCLOG2: TABS FOCUS 2");
-			System.out.println("LCCLOG MOBCLIX: onWindowFocusChanged SHOW");
-			MobclixHelper.showBannerAd(MobclixHelper.getBannerAdviewWrapper(App), removeAds, this, App);
-			//System.out.println("LCCLOG2: TABS FOCUS 3");
-		}
-	}
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+	  //System.out.println("LCCLOG2: TABS FOCUS 1");
+    System.out.println("LCCLOG MOBCLIX: onWindowFocusChanged hasFocus=" + hasFocus + ", isForceBannerAdOnFailedLoad=" + App.isForceBannerAdOnFailedLoad());
+    if (MobclixHelper.isShowAds(App) && hasFocus && App.isForceBannerAdOnFailedLoad())
+    {
+		//System.out.println("LCCLOG2: TABS FOCUS 2");
+	  System.out.println("LCCLOG MOBCLIX: onWindowFocusChanged SHOW");
+      MobclixHelper.showBannerAd(MobclixHelper.getBannerAdviewWrapper(App), removeAds, this, App);
+		//System.out.println("LCCLOG2: TABS FOCUS 3");
+    }
+  }
 
-	private BroadcastReceiver lccLoggingInInfoReceiver = new BroadcastReceiver() {
+  private BroadcastReceiver lccLoggingInInfoReceiver = new BroadcastReceiver() {
 		@Override
 		public void onReceive(Context context, final Intent intent) {
 			new Handler().post(new Runnable() {
 				public void run() {
-					if (getTabHost().getCurrentTabTag().equals("tab2") && App.isLiveChess() && !intent.getExtras().getBoolean("enable")) {
-						if (MobclixHelper.isShowAds(App) && App.getLccHolder().isConnected() && !App.getLccHolder().isConnectingInProgress()) {
+					if (getTabHost().getCurrentTabTag().equals("tab2") && App.isLiveChess() && !intent.getExtras().getBoolean("enable"))
+					{
+						if (MobclixHelper.isShowAds(App) && App.getLccHolder().isConnected() && !App.getLccHolder().isConnectingInProgress())
+						{
 							MobclixHelper.showBannerAd(MobclixHelper.getBannerAdviewWrapper(App), removeAds, Tabs.this, App);
-						}
+			            }
 					}
 				}
 			});
--- src/main/java/com/facebook/android/LoginButton.java
+++ src/main/java/com/facebook/android/LoginButton.java
@@ -16,139 +16,118 @@
 
 package com.facebook.android;
 
+import com.chess.R;
+import com.chess.core.MainApp;
+import com.facebook.android.BaseRequestListener;
+import com.facebook.android.SessionEvents.AuthListener;
+import com.facebook.android.SessionEvents.LogoutListener;
+import com.facebook.android.Facebook.DialogListener;
+
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
-import com.chess.R;
-import com.chess.core.MainApp;
-import com.facebook.android.Facebook.DialogListener;
-import com.facebook.android.SessionEvents.AuthListener;
-import com.facebook.android.SessionEvents.LogoutListener;
+public class LoginButton extends ImageButton {
+    
+    private Facebook mFb;
+    private Handler mHandler;
+    private SessionListener mSessionListener = new SessionListener();
+    private String[] mPermissions;
+    
+    public LoginButton(Context context) {
+        super(context);
+    }
+    
+    public LoginButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    
+    public LoginButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+    
+    public void init(final Facebook fb, final String[] permissions) {
+        mFb = fb;
+        mPermissions = permissions;
+        mHandler = new Handler();
+        
+        setBackgroundColor(Color.TRANSPARENT);
+        setAdjustViewBounds(true);
+        setImageResource(fb.isSessionValid() ?
+                         R.drawable.logout_button : 
+                         R.drawable.login_button);
+        drawableStateChanged();
+        
+        SessionEvents.addAuthListener(mSessionListener);
+        SessionEvents.addLogoutListener(mSessionListener);
+        setOnClickListener(new ButtonOnClickListener());
+    }
+    
+    private final class ButtonOnClickListener implements OnClickListener {
+        
+        public void onClick(View arg0) {
+            if (mFb.isSessionValid()) {
+                SessionEvents.onLogoutBegin();
+                AsyncFacebookRunner asyncRunner = new AsyncFacebookRunner(mFb);
+                asyncRunner.logout(getContext(), new LogoutRequestListener());
+            } else {
+                mFb.authorize(getContext(), MainApp.APP_ID, mPermissions,
+                        new LoginDialogListener());
+            }
+        }
+    }
 
-public class LoginButton extends /* Image */Button {
+    private final class LoginDialogListener implements DialogListener {
+        public void onComplete(Bundle values) {
+            SessionEvents.onLoginSuccess();
+        }
 
-	private Facebook mFb;
-	private Handler mHandler;
-	private final SessionListener mSessionListener = new SessionListener();
-	private String[] mPermissions;
+        public void onFacebookError(FacebookError error) {
+            SessionEvents.onLoginError(error.getMessage());
+        }
+        
+        public void onError(DialogError error) {
+            SessionEvents.onLoginError(error.getMessage());
+        }
 
-	public LoginButton(Context context) {
-		super(context);
-	}
+        public void onCancel() {
+            SessionEvents.onLoginError("Action Canceled");
+        }
+    }
+    
+    private class LogoutRequestListener extends BaseRequestListener {
+        public void onComplete(String response) {
+            // callback should be run in the original thread, 
+            // not the background thread
+            mHandler.post(new Runnable() {
+                public void run() {
+                    SessionEvents.onLogoutFinish();
+                }
+            });
+        }
+    }
+    
+    private class SessionListener implements AuthListener, LogoutListener {
+        
+        public void onAuthSucceed() {
+            setImageResource(R.drawable.logout_button);
+            SessionStore.save(mFb, getContext());
+        }
 
-	public LoginButton(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
-
-	public LoginButton(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-	}
-
-	public void init(final Facebook fb, final String[] permissions) {
-		mFb = fb;
-		mPermissions = permissions;
-		mHandler = new Handler();
-
-//		setBackgroundColor(Color.TRANSPARENT);
-//		setAdjustViewBounds(true);
-//		setImageResource(fb.isSessionValid() ? R.drawable.logout_button
-//				: R.drawable.login_button_selector);
-		setBackgroundResource(fb.isSessionValid() ? R.drawable.button_f_logout_selector
-				: R.drawable.button_f_login_selector);
-		setText(getResources().getString(
-				fb.isSessionValid() ? R.string.logout
-						: R.string.connect_with_facebook));
-
-		invalidate();
-//		drawableStateChanged();
-
-		SessionEvents.addAuthListener(mSessionListener);
-		SessionEvents.addLogoutListener(mSessionListener);
-		setOnClickListener(new ButtonOnClickListener());
-	}
-
-	private final class ButtonOnClickListener implements OnClickListener {
-
-		@Override
-		public void onClick(View arg0) {
-			if (mFb.isSessionValid()) {
-				SessionEvents.onLogoutBegin();
-				AsyncFacebookRunner asyncRunner = new AsyncFacebookRunner(mFb);
-				asyncRunner.logout(getContext(), new LogoutRequestListener());
-			} else {
-				mFb.authorize(getContext(), MainApp.APP_ID, mPermissions,
-						new LoginDialogListener());
-			}
-		}
-	}
-
-	private final class LoginDialogListener implements DialogListener {
-		@Override
-		public void onComplete(Bundle values) {
-			SessionEvents.onLoginSuccess();
-		}
-
-		@Override
-		public void onFacebookError(FacebookError error) {
-			SessionEvents.onLoginError(error.getMessage());
-		}
-
-		@Override
-		public void onError(DialogError error) {
-			SessionEvents.onLoginError(error.getMessage());
-		}
-
-		@Override
-		public void onCancel() {
-			SessionEvents.onLoginError("Action Canceled");
-		}
-	}
-
-	private class LogoutRequestListener extends BaseRequestListener {
-		@Override
-		public void onComplete(String response) {
-			// callback should be run in the original thread,
-			// not the background thread
-			mHandler.post(new Runnable() {
-				@Override
-				public void run() {
-					SessionEvents.onLogoutFinish();
-				}
-			});
-		}
-	}
-
-	private class SessionListener implements AuthListener, LogoutListener {
-
-		@Override
-		public void onAuthSucceed() {
-			setBackgroundResource(R.drawable.button_f_logout_selector);// ImageResource(R.drawable.button_f_logout_selector);
-			setText(getResources().getString(R.string.logout));
-
-			SessionStore.save(mFb, getContext());
-		}
-
-		@Override
-		public void onAuthFail(String error) {
-		}
-
-		@Override
-		public void onLogoutBegin() {
-		}
-
-		@Override
-		public void onLogoutFinish() {
-			SessionStore.clear(getContext());
-			setBackgroundResource(R.drawable.button_f_login_selector);
-			setText(getResources().getString(R.string.connect_with_facebook));
-
-//			invalidate();
-//			setImageResource(R.drawable.login_button_selector);
-		}
-	}
-
+        public void onAuthFail(String error) {
+        }
+        
+        public void onLogoutBegin() {           
+        }
+        
+        public void onLogoutFinish() {
+            SessionStore.clear(getContext());
+            setImageResource(R.drawable.login_button);
+        }
+    }
+    
 }
