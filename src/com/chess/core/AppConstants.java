6f6a07eccafbdf0531a1b38c227401ef21500f89
vmalykhin <vm@.(none)>
2012-01-26 23:59:54 +0200

    Android: rename old variables, change visibility, add access methods, extract constants

---
+++ src/com/chess/core/AppConstants.java
@@ -0,0 +1,18 @@
+package com.chess.core;
+
+public class AppConstants {
+	public static String ENTER_FROM_NOTIFICATION = "enter-from-notification";
+	public static String GAME_ID = "game-id";
+	public static String TIMESTAMP = "time-stamp";
+	public static String GAME_MODE = "game-mode";
+	public static String MESSAGE = "message";
+	public static String ACTION_SHOW_GAME_END_POPUP = "com.chess.lcc.android-show-game-end-popup";
+	public static String FINISHABLE = "finishable";
+	public static String LIVE_CHESS = "live-chess";
+	public static String OBJECT = "object";
+	public static String TITLE = "title";
+	public static String TAB_INDEX	= "tab-index";
+	public static String VIDEO_SKILL_LEVEL = "video-skill-level";
+	public static String VIDEO_CATEGORY = "video-category";
+	public static String ENABLE_LIVE_CONNECTING_INDICATOR = "enable-live-connectiong-indicator";
+}
