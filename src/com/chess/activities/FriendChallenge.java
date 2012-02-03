a9d8cb7edd6381287b09eada5df160df619af454
Roger <sent2roger@gmail.com>
2012-02-03 09:35:36 +0600

    FriendChallenge class refactored

--- src/com/chess/activities/FriendChallenge.java
+++ src/com/chess/activities/FriendChallenge.java
@@ -11,7 +11,6 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.*;
-import android.widget.AutoCompleteTextView.Validator;
 import com.chess.R;
 import com.chess.core.AppConstants;
 import com.chess.core.CoreActivity;
@@ -25,16 +24,32 @@ import com.chess.utilities.ChessComApiParser;
 import com.chess.utilities.MyProgressDialog;
 import com.flurry.android.FlurryAgent;
 
-public class FriendChallenge extends CoreActivity {
+public class FriendChallenge extends CoreActivity implements OnClickListener {
 	private Spinner iplayas, dayspermove, friends;
 	private AutoCompleteTextView initialTime;
 	private AutoCompleteTextView bonusTime;
 	private CheckBox isRated;
 	private RadioButton chess960;
 
+	private InitialTimeTextWatcher initialTimeTextWatcher;
+	private InitialTimeValidator initialTimeValidator;
+	private BonusTimeTextWatcher bonusTimeTextWatcher;
+	private BonusTimeValidator bonusTimeValidator;
+
+	private int[] daysArr = new int[]{
+			1,
+			2,
+			3,
+			5,
+			7,
+			14
+	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+
 		if (mainApp.isLiveChess()) {
 			setContentView(R.layout.challengeafriendlive);
 		} else {
@@ -43,175 +58,24 @@ public class FriendChallenge extends CoreActivity {
 			chess960 = (RadioButton) findViewById(R.id.chess960);
 			iplayas = (Spinner) findViewById(R.id.iplayas);
 		}
+
+		init();
+
 		friends = (Spinner) findViewById(R.id.friend);
 		isRated = (CheckBox) findViewById(R.id.ratedGame);
 		initialTime = (AutoCompleteTextView) findViewById(R.id.initialTime);
 		bonusTime = (AutoCompleteTextView) findViewById(R.id.bonusTime);
 		if (mainApp.isLiveChess()) {
 			initialTime.setText(mainApp.getSharedData().getString(AppConstants.CHALLENGE_INITIAL_TIME, "5"));
-			initialTime.addTextChangedListener(new TextWatcher() {
-				@Override
-				public void onTextChanged(CharSequence s, int start, int before, int count) {
-					initialTime.performValidation();
-				}
-
-				@Override
-				public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-				}
-
-				@Override
-				public void afterTextChanged(Editable s) {
-					initialTime.performValidation();
-				}
-			});
-			initialTime.setValidator(new Validator() {
-				@Override
-				public boolean isValid(CharSequence text) {
-					final String textString = new String(text.toString().trim());
-					final Integer initialTime = new Integer(textString);
-					if (!textString.equals("") && initialTime >= 1 && initialTime <= 120) {
-						return true;
-					} else {
-						return false;
-					}
-				}
-
-				@Override
-				public CharSequence fixText(CharSequence invalidText) {
-					return mainApp.getSharedData().getString(AppConstants.CHALLENGE_INITIAL_TIME, "5");
-				}
-			});
+			initialTime.addTextChangedListener(initialTimeTextWatcher);
+			initialTime.setValidator(initialTimeValidator);
 			initialTime.setOnEditorActionListener(null);
+			
 			bonusTime.setText(mainApp.getSharedData().getString(AppConstants.CHALLENGE_BONUS_TIME, "0"));
-			bonusTime.addTextChangedListener(new TextWatcher() {
-				@Override
-				public void onTextChanged(CharSequence s, int start, int before, int count) {
-					bonusTime.performValidation();
-				}
-
-				@Override
-				public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-				}
-
-				@Override
-				public void afterTextChanged(Editable s) {
-					bonusTime.performValidation();
-				}
-			});
-			bonusTime.setValidator(new Validator() {
-				@Override
-				public boolean isValid(CharSequence text) {
-					final String textString = new String(text.toString().toString());
-					final Integer bonusTime = new Integer(textString);
-					if (!textString.equals("") && bonusTime >= 0 && bonusTime <= 60) {
-						return true;
-					} else {
-						return false;
-					}
-				}
-
-				@Override
-				public CharSequence fixText(CharSequence invalidText) {
-					return mainApp.getSharedData().getString(AppConstants.CHALLENGE_BONUS_TIME, "0");
-				}
-			});
+			bonusTime.addTextChangedListener(bonusTimeTextWatcher );
+			bonusTime.setValidator(bonusTimeValidator);
 		}
-		findViewById(R.id.createchallenge).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (friends.getCount() == 0) {
-					return;
-				}
-				if (mainApp.isLiveChess()) {
-					if (initialTime.getText().toString().length() < 1 || bonusTime.getText().toString().length() < 1) {
-						initialTime.setText("10");
-						bonusTime.setText("0");
-					}
-					/*PieceColor color;
-							  switch(iplayas.getSelectedItemPosition())
-							  {
-								case 0:
-								  color = PieceColor.UNDEFINED;
-								  break;
-								case 1:
-								  color = PieceColor.WHITE;
-								  break;
-								case 2:
-								  color = PieceColor.BLACK;
-								  break;
-								default:
-								  color = PieceColor.UNDEFINED;
-								  break;
-							  }*/
-					final Boolean rated = isRated.isChecked();
-					final Integer initialTimeInteger = new Integer(initialTime.getText().toString());
-					final Integer bonusTimeInteger = new Integer(bonusTime.getText().toString());
-					final GameTimeConfig gameTimeConfig = new GameTimeConfig(initialTimeInteger * 60 * 10, bonusTimeInteger * 10);
-					final Integer minRating = null;
-					final Integer maxRating = null;
-					final Challenge challenge = LiveChessClientFacade.createCustomSeekOrChallenge(
-							lccHolder.getUser(), friends.getSelectedItem().toString().trim(), PieceColor.UNDEFINED, rated, gameTimeConfig,
-							minRating, maxRating);
-					if (appService != null) {
-						FlurryAgent.onEvent("Challenge Created", null);
-						lccHolder.getAndroid().runSendChallengeTask(
-								//progressDialog = MyProgressDialog.show(FriendChallenge.this, null, getString(R.string.creating), true),
-								null,
-								challenge
-						);
-						Update(1);
-					}
-				} else {
-					int color = iplayas.getSelectedItemPosition();
-					int days = 1;
-					int israted = 0;
-					int gametype = 0;
-					switch (dayspermove.getSelectedItemPosition()) {
-						case 0:
-							days = 1;
-							break;
-						case 1:
-							days = 2;
-							break;
-						case 2:
-							days = 3;
-							break;
-						case 3:
-							days = 5;
-							break;
-						case 4:
-							days = 7;
-							break;
-						case 5:
-							days = 14;
-							break;
-						default:
-							break;
-					}
-					if (isRated.isChecked()) {
-						israted = 1;
-					} else {
-						israted = 0;
-					}
-					if (chess960.isChecked()) {
-						gametype = 2;
-					}
-					String query = "http://www." + LccHolder.HOST + "/api/echess_new_game?id=" + mainApp.getSharedData().getString(AppConstants.USER_TOKEN, "") +
-							"&timepermove=" + days +
-							"&iplayas=" + color +
-							"&israted=" + israted +
-							"&game_type=" + gametype +
-							"&opponent=" + friends.getSelectedItem().toString().trim();
-					if (appService != null) {
-						appService.RunSingleTask(1,
-								query,
-								progressDialog = new MyProgressDialog(ProgressDialog
-										.show(FriendChallenge.this, null, getString(R.string.creating), true))
-						);
-					}
-				}
-			}
-		});
+		findViewById(R.id.createchallenge).setOnClickListener(this);
 	}
 
 	@Override
@@ -284,4 +148,154 @@ public class FriendChallenge extends CoreActivity {
 			startActivity(new Intent(this, Tabs.class));
 		}
 	}
+
+	private void init(){
+		initialTimeTextWatcher = new InitialTimeTextWatcher();
+		initialTimeValidator = new InitialTimeValidator();
+		bonusTimeTextWatcher = new BonusTimeTextWatcher();
+		bonusTimeValidator = new BonusTimeValidator();
+	}
+
+	@Override
+	public void onClick(View view) {
+		if(view.getId() == R.id.createchallenge){
+			if (friends.getCount() == 0) {
+				return;
+			}
+			if (mainApp.isLiveChess()) {
+				if (initialTime.getText().toString().length() < 1 || bonusTime.getText().toString().length() < 1) {
+					initialTime.setText("10");
+					bonusTime.setText("0");
+				}
+				/*PieceColor color;
+											  switch(iplayas.getSelectedItemPosition())
+											  {
+												case 0:
+												  color = PieceColor.UNDEFINED;
+												  break;
+												case 1:
+												  color = PieceColor.WHITE;
+												  break;
+												case 2:
+												  color = PieceColor.BLACK;
+												  break;
+												default:
+												  color = PieceColor.UNDEFINED;
+												  break;
+											  }*/
+				final Boolean rated = isRated.isChecked();
+				final Integer initialTimeInteger = new Integer(initialTime.getText().toString());
+				final Integer bonusTimeInteger = new Integer(bonusTime.getText().toString());
+				final GameTimeConfig gameTimeConfig = new GameTimeConfig(initialTimeInteger * 60 * 10, bonusTimeInteger * 10);
+				final Integer minRating = null;
+				final Integer maxRating = null;
+				final Challenge challenge = LiveChessClientFacade.createCustomSeekOrChallenge(
+						lccHolder.getUser(), friends.getSelectedItem().toString().trim(), PieceColor.UNDEFINED, rated, gameTimeConfig,
+						minRating, maxRating);
+				if (appService != null) {
+					FlurryAgent.onEvent("Challenge Created", null);
+					lccHolder.getAndroid().runSendChallengeTask(
+							//progressDialog = MyProgressDialog.show(FriendChallenge.this, null, getString(R.string.creating), true),
+							null,
+							challenge
+					);
+					Update(1);
+				}
+			} else {
+				int color = iplayas.getSelectedItemPosition();
+				int days = 1;
+				days = daysArr[dayspermove.getSelectedItemPosition()];
+				int israted = 0;
+				int gametype = 0;
+
+				if (isRated.isChecked()) {
+					israted = 1;
+				} else {
+					israted = 0;
+				}
+				if (chess960.isChecked()) {
+					gametype = 2;
+				}
+				String query = "http://www." + LccHolder.HOST + "/api/echess_new_game?id=" + mainApp.getSharedData().getString(AppConstants.USER_TOKEN, "") +
+						"&timepermove=" + days +
+						"&iplayas=" + color +
+						"&israted=" + israted +
+						"&game_type=" + gametype +
+						"&opponent=" + friends.getSelectedItem().toString().trim();
+				if (appService != null) {
+					appService.RunSingleTask(1,
+							query,
+							progressDialog = new MyProgressDialog(ProgressDialog
+									.show(FriendChallenge.this, null, getString(R.string.creating), true))
+					);
+				}
+			}
+		}
+	}
+
+
+	private class InitialTimeTextWatcher implements TextWatcher {
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before, int count) {
+			initialTime.performValidation();
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+		}
+
+		@Override
+		public void afterTextChanged(Editable s) {
+			initialTime.performValidation();
+		}
+	}
+
+	private class InitialTimeValidator implements AutoCompleteTextView.Validator {
+		@Override
+		public boolean isValid(CharSequence text) {
+			final String textString = text.toString().trim();
+			final Integer initialTime = new Integer(textString);
+			return !textString.equals("") && initialTime >= 1 && initialTime <= 120;
+		}
+
+		@Override
+		public CharSequence fixText(CharSequence invalidText) {
+			return mainApp.getSharedData().getString(AppConstants.CHALLENGE_INITIAL_TIME, "5");
+		}
+	}
+
+	private class BonusTimeTextWatcher implements TextWatcher {
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before, int count) {
+			bonusTime.performValidation();
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+		}
+
+		@Override
+		public void afterTextChanged(Editable s) {
+			bonusTime.performValidation();
+		}
+	}
+
+	private class BonusTimeValidator implements AutoCompleteTextView.Validator {
+		@Override
+		public boolean isValid(CharSequence text) {
+			final String textString = new String(text.toString().toString());
+			final Integer bonusTime = new Integer(textString);
+			if (!textString.equals("") && bonusTime >= 0 && bonusTime <= 60) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		@Override
+		public CharSequence fixText(CharSequence invalidText) {
+			return mainApp.getSharedData().getString(AppConstants.CHALLENGE_BONUS_TIME, "0");
+		}
+	}
+
 }
