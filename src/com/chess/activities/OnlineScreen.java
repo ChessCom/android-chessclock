94dcc6ef6bb3ab22ef7e5cebb59bb17b3ca8a638
Roger <sent2roger@gmail.com>
2012-02-21 06:56:00 +0600

    SignIn Screen adjustment changes  - Edit text adjustment changes  - Logo changed  - Grey button color changed

---
+++ src/com/chess/activities/OnlineScreen.java
@@ -0,0 +1,16 @@
+package com.chess.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * OnlineScreen class
+ *
+ * @author alien_roger
+ * @created at: 08.02.12 7:12
+ */
+public class OnlineScreen extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
