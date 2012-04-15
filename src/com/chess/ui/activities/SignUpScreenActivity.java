41f907b3739158c7420b45105ec2c81b279937f6
Roger <sent2roger@gmail.com>
2012-04-16 06:37:40 +0700

    Android:
    Facebook login/auth via SingleSign on added.

--- src/com/chess/ui/activities/SignUpScreenActivity.java
+++ src/com/chess/ui/activities/SignUpScreenActivity.java
@@ -278,4 +278,10 @@ public class SignUpScreenActivity extends CoreActivityActionBar implements View.
         }
     }
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		facebook.authorizeCallback(requestCode, resultCode, data);
+	}
+
 }
