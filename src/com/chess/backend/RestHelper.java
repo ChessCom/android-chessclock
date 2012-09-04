edf6eebd3384e3c83bd136ae533f6ac205dc15ee
vmalykhin <vm@.(none)>
2012-09-04 22:06:52 +0300

    Android: use v3/get_echess_current_games

--- src/com/chess/backend/RestHelper.java
+++ src/com/chess/backend/RestHelper.java
@@ -70,7 +70,7 @@ public class RestHelper {
 	public static final String ECHESS_CHALLENGES = BASE_URL + API + "/echess_challenges";
 	public static final String ECHESS_CHALLENGES_V2 = BASE_URL + API_V2 + "/echess_challenges";
 	public static final String ECHESS_NEW_GAME = BASE_URL + API + "/echess_new_game";
-	public static final String ECHESS_CURRENT_GAMES = BASE_URL + API_V2 + "/get_echess_current_games";
+	public static final String ECHESS_CURRENT_GAMES = BASE_URL + API_V3 + "/get_echess_current_games";
 	public static final String ECHESS_FINISHED_GAMES = BASE_URL + API_V2 + "/get_echess_finished_games";
 	public static final String ECHESS_INFO = BASE_URL + API_V2 + "/get_echess_info";
 
