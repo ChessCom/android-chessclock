1a8f0101eb964267978fabd0f2b57420516bc475
vmalykhin <vm@.(none)>
2013-03-26 16:56:16 +0200

    Challenge and ConnectionFailure popups does not appear when user returns from background

--- src/com/chess/ui/activities/BaseFragmentActivity.java
+++ src/com/chess/ui/activities/BaseFragmentActivity.java
@@ -118,27 +118,26 @@ public abstract class BaseFragmentActivity extends FragmentActivity implements P
 	@Override
 	protected void onResume() {
 		super.onResume();
-		isPaused = false;
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		isPaused = true;
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
+		isPaused = false;
 		EasyTracker.getInstance().activityStart(this);
 	}
 
 	@Override
 	protected void onStop() {
 		super.onStop();
+		isPaused = true;
 		EasyTracker.getInstance().activityStop(this);
 	}
-
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
