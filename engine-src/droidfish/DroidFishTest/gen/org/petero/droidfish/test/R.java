902e5e05794906a817535f6b6508a9fc544f8e3f
vmalykhin <vladimir.malykhin@gmail.com>
2013-07-11 14:19:20 +0300

    remove

--- engine-src/droidfish/DroidFishTest/gen/org/petero/droidfish/test/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package org.petero.droidfish.test;
-
-/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
-public final class R {
-}
