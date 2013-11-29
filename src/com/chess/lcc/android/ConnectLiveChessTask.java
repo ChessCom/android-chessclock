9c18f70f7724369badd4fc9fa3b5f98f79cb99e0
vmalykhin <vladimir.malykhin@gmail.com>
2013-11-29 02:38:52 +0200

    * use new LCC API, use new WS/WSS/HTTP configs and fallbacks, remove old code  * get ConnectionUrls dynamically  * update LCC  * add logger for Throwables  * fix forceLog() for there is no connection case  * add Notify log marker

--- src/com/chess/lcc/android/ConnectLiveChessTask.java
+++ src/com/chess/lcc/android/ConnectLiveChessTask.java
@@ -3,20 +3,19 @@ package com.chess.lcc.android;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.util.Log;
 import com.chess.backend.RestHelper;
 import com.chess.backend.interfaces.TaskUpdateInterface;
 import com.chess.backend.tasks.AbstractUpdateTask;
 import com.chess.live.client.*;
 import com.chess.live.client.impl.HttpClientProvider;
-import com.chess.live.util.config.Config;
+import com.chess.statics.AppData;
 import com.chess.statics.StaticData;
-import com.chess.statics.Symbol;
 import com.chess.utilities.LogMe;
 import org.eclipse.jetty.client.HttpClient;
 
 import java.io.IOException;
-import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * ConnectLiveChessTask class
@@ -27,44 +26,27 @@ import java.util.LinkedHashSet;
 public class ConnectLiveChessTask extends AbstractUpdateTask<LiveChessClient, Void> {
 
 	private static final String TAG = "LCCLOG-ConnectLiveChessTask";
-	/*public static final String PKCS_12 = "PKCS12";
-	public static final String TESTTEST = "testtest";
-	public static final String KEY_FILE_NAME = "chesscom.pkcs12";*/
 
 	//static MemoryUsageMonitor muMonitor = new MemoryUsageMonitor(15);
 
 	public static final String LIVE_HOST_PRODUCTION = "chess.com";
 	public static final String LIVE_HOST_TEST = "chess-2.com";
 
-	//	public static final String AUTH_URL = RestHelper.CMD_LOGIN + "?username=%s&password=%s";
-	final static Config CONFIG = new Config(Symbol.EMPTY, "assets/my.properties", true);
-	public static final String CONFIG_URI =
-			Config.get(CONFIG.getString("live.chess.client.demo.chat_generator.connection.bayeux.uri"), "/cometd");
+	private static final int MAX_BACKOFF_INTERVAL = 10000;
+	private static final long WS_CONNECT_TIMEOUT = 10000L;
+	private static final int WS_MAX_MESSAGE_SIZE = 1024 * 1024;
 
-	/*public static final String HOST = "10.0.2.2";
-	  public static final String AUTH_URL = "http://" + HOST + "/api/v2/login?username=%s&password=%s";
-	  public static final String CONFIG_BAYEUX_HOST = HOST;*/
-
-	//Config.get(CONFIG.getString("live.chess.client.demo.chat_generator.connection.bayeux.host"), "live.chess-4.com");
-
-	public static Integer PORT = 80;
-	public static Integer PORT_SECURED = 443;
-
-	/*public static final String CONFIG_AUTH_KEY =
-			Config.get(CONFIG.getString("live.chess.client.demo.chat_generator.connection.user1.authKey"),
-					"FIXED_PHPSESSID_WEBTIDE_903210957432054387723");*/
-
-	public static final int MAX_BACKOFF_INTERVAL = 10000;
-	public static final int WEB_SOCKET_MAX_MESSAGE_SIZE = 1048576; // 1mb
-
-	private boolean useSessionId;
-
+	private boolean useCurrentCredentials;
 	private LccHelper lccHelper;
 
-	public ConnectLiveChessTask(TaskUpdateInterface <LiveChessClient> taskFace, boolean useSessionId, LccHelper lccHelper) {
+	public  ConnectLiveChessTask(TaskUpdateInterface<LiveChessClient> taskFace, boolean useCurrentCredentials, LccHelper lccHelper) {
+		this(taskFace, lccHelper);
+		this.useCurrentCredentials = useCurrentCredentials;
+	}
+
+	public ConnectLiveChessTask(TaskUpdateInterface<LiveChessClient> taskFace, LccHelper lccHelper) {
 		super(taskFace);
 		this.lccHelper = lccHelper;
-		this.useSessionId = useSessionId;
 	}
 
 	@Override
@@ -81,46 +63,11 @@ public class ConnectLiveChessTask extends AbstractUpdateTask<LiveChessClient, Vo
 			}
 			versionName += ", OS: " + android.os.Build.VERSION.RELEASE + ", " + android.os.Build.MODEL;
 
-//			InputStream keyStoreInputStream = context.getAssets().open(LccHelper.KEY_FILE_NAME);
+			AppData appData = new AppData(context);
 
-			LinkedHashSet<ClientTransport> transports = new LinkedHashSet<ClientTransport>();
-			int port = PORT;
-
-			if (Build.VERSION.SDK_INT == Build.VERSION_CODES.FROYO) { // Android 2.2
-				LogMe.dl(TAG, "Support HTTP Live transport");
-				transports.add(ClientTransport.HTTP);
-
-			} else {
-				LogMe.dl(TAG, "Support WS and HTTP Live transports");
-				transports.add(ClientTransport.WS);
-				transports.add(ClientTransport.HTTP);
-			}
-
+			LogMe.forceLog(TAG, "User " + appData.getUsername() + ", " + versionName, context);
 			LogMe.dl(TAG, "Start Chess.Com LCC ");
-			LogMe.dl(TAG, "Connecting to: " + getConfigBayeuxHost() + ":" + port);
-
-			item = LiveChessClientFacade.createClient(getAuthUrl(), getConfigBayeuxHost(),
-					port, CONFIG_URI); // todo: check incorrect port connection failure
-			item.setClientInfo("Android", versionName, "No-Key");
-
-			item.setSupportedClientFeature(ClientFeature.AnnounceService, true);
-			item.setSupportedClientFeature(ClientFeature.AdminService, true); // UPDATELCC todo: check
-
-			item.setSupportedClientFeature(ClientFeature.GenericGameSupport, true);
-			item.setSupportedClientFeature(ClientFeature.GenericChatSupport, true);
-			item.setSupportedClientFeature(ClientFeature.GameObserve, true);
-
-			item.setMaxBackoffInterval(MAX_BACKOFF_INTERVAL);
-			item.setWebSocketMaxMessageSize(WEB_SOCKET_MAX_MESSAGE_SIZE);
-
-			//PublicChatsBasic
-			//PublicChatsFull
-			//PrivateChats
-			//MultiGames
-			//MultiGameObserve
-			//Tournaments
-			//ExamineBoards
-			//PingService
+			LogMe.dl(TAG, "Connecting to: " + getConfigBayeuxHost());
 
 			HttpClient httpClient = HttpClientProvider.getHttpClient(HttpClientProvider.DEFAULT_CONFIGURATION, false);
 
@@ -140,40 +87,79 @@ public class ConnectLiveChessTask extends AbstractUpdateTask<LiveChessClient, Vo
 			httpClient.setConnectTimeout(10000); // 75000 is default
 			httpClient.setTimeout(10000); // 320000 is default
 
-			/*httpClient.setKeyStoreType(PKCS_12);
-			httpClient.setTrustStoreType(PKCS_12);
-			httpClient.setKeyManagerPassword(TESTTEST);
-			httpClient.setKeyStoreInputStream(keyStoreInputStream);
-			httpClient.setKeyStorePassword(TESTTEST);
-			httpClient.setTrustStoreInputStream(keyStoreInputStream);
-			httpClient.setTrustStorePassword(TESTTEST);*/
+			List<ConnectionConfiguration> conConfs = new LinkedList<ConnectionConfiguration>();
 
-			//transports.add(ClientTransport.HTTP); // test
-			//transports.add(ClientTransport.WS); // test
+			if (Build.VERSION.SDK_INT == Build.VERSION_CODES.FROYO) { // Android 2.2
+				LogMe.dl(TAG, "Support HTTP Live transport");
+				conConfs.add(createHttpConnectionConfig(httpClient));
 
-			item.setClientTransports(transports);
+			} else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) { // prior to 4.1
+				LogMe.dl(TAG, "Support WS and HTTP Live transports");
+				conConfs.add(createWSConnectionConfig());
+				conConfs.add(createHttpConnectionConfig(httpClient));
 
-			item.setHttpClient(httpClient);
+			} else { // 4.1+
+				LogMe.dl(TAG, "Support WSS and HTTP Live transports");
+				conConfs.add(createWSSConnectionConfig());
+				conConfs.add(createHttpConnectionConfig(httpClient));
+			}
+
+			item = LiveChessClientFacade.createClient(getAuthUrl(), conConfs);
+
+			item.setClientInfo("Android", versionName, "No-Key");
+
+			item.setSupportedClientFeature(ClientFeature.AnnounceService, true);
+			item.setSupportedClientFeature(ClientFeature.AdminService, true); // UPDATELCC todo: check
+
+			item.setSupportedClientFeature(ClientFeature.GenericGameSupport, true);
+			item.setSupportedClientFeature(ClientFeature.GenericChatSupport, true);
+
+			//PublicChatsBasic
+			//PublicChatsFull
+			//PrivateChats
+			//MultiGames
+			//GameObserve
+			//MultiGameObserve
+			//Tournaments
+			//ExamineBoards
+			//PingService
+
+			item.setMaxBackoffInterval(MAX_BACKOFF_INTERVAL);
 
 			httpClient.start();
-		} catch (PackageManager.NameNotFoundException e) {
-			result = StaticData.UNKNOWN_ERROR;
-//            e.printStackTrace();
-			Log.e(TAG, "Probably can't find pks12 file in assets" + e.toString());
 		} catch (IOException e) {
 //            e.printStackTrace();
 			result = StaticData.NO_NETWORK;
-			Log.e(TAG, e.toString());
+			LogMe.dl(TAG, e.toString());
 		} catch (Exception e) {
 			result = StaticData.UNKNOWN_ERROR;
 			throw new LiveChessClientException("Unable to initialize HttpClient", e);
 		}
 
-		//lccHelper.setNetworkTypeName(null); // todo: probably reset networkTypeName somewhere else
+		//lccHelper.updateNetworkType(null); // todo: probably reset networkTypeName somewhere else?
+		//lccHelper.setConnectionFailure(false);
 		lccHelper.setLiveChessClient(item);
-		lccHelper.performConnect(useSessionId);
+		lccHelper.performConnect(useCurrentCredentials);
 		return StaticData.RESULT_OK;
 	}
+
+	private SimpleHttpConnectionConfiguration createHttpConnectionConfig(HttpClient httpClient) {
+		return new SimpleHttpConnectionConfiguration(httpClient, ClientTransport.HTTP, getHttpConnectionUrl());
+	}
+
+	private SimpleWebSocketConnectionConfiguration createWSConnectionConfig() {
+		return new SimpleWebSocketConnectionConfiguration(ClientTransport.WS, getWSConnectionUrl(), WS_CONNECT_TIMEOUT,
+				WS_MAX_MESSAGE_SIZE, false);
+	}
+
+	private SimpleWebSocketConnectionConfiguration createWSSConnectionConfig() {
+		return new SimpleWebSocketConnectionConfiguration(ClientTransport.WSS, getWSSConnectionUrl(), WS_CONNECT_TIMEOUT,
+				WS_MAX_MESSAGE_SIZE, false);
+	}
+
+	/*private SimpleHttpConnectionConfiguration createHttpsConnectionConfig(HttpClient httpClient) {
+		return new SimpleHttpConnectionConfiguration(httpClient, ClientTransport.HTTPS, HTTPS_URL);
+	}*/
 
 	private String getLiveHost() {
 		String liveHost = RestHelper.getInstance().HOST.equals(RestHelper.HOST_PRODUCTION) ? LIVE_HOST_PRODUCTION : LIVE_HOST_TEST;
@@ -187,5 +173,20 @@ public class ConnectLiveChessTask extends AbstractUpdateTask<LiveChessClient, Vo
 	private String getConfigBayeuxHost() {
 		return "live." + getLiveHost();
 	}
+	private String getWSConnectionUrl() {
+		return "ws://" + getConfigBayeuxHost() + ":80/cometd";
+	}
+
+	private String getWSSConnectionUrl() {
+		return "wss://" + getConfigBayeuxHost() + ":443/cometd";
+	}
+
+	private String getHttpConnectionUrl() {
+		return "http://" + getConfigBayeuxHost() + ":80/cometd";
+	}
+
+	/*private String getHttpsConnectionUrl() {
+		return "https://" + getConfigBayeuxHost() + ":443/cometd";
+	}*/
 
 }
--- src/com/chess/utilities/LogMe.java
+++ src/com/chess/utilities/LogMe.java
@@ -1,7 +1,10 @@
 package com.chess.utilities;
 
+import android.content.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,11 +19,25 @@ public class LogMe {
 	private static final String DEBUG_LIVE_TAG = "LCCLOG";
 	private static final String TAG_MESSAGE_SEPARATOR = ": ";
 
+	private static final Marker markerNotify = MarkerFactory.getMarker("NOTIFY");
+
+	public static void forceLog(String tag, String message, Context context) {
+		if (context != null && AppUtils.isNetworkAvailable(context)) {
+			LOG.debug(markerNotify, tag + TAG_MESSAGE_SEPARATOR + message); // markerNotify sends mail buffer immediately
+		} else {
+			dl(tag, message);
+		}
+	}
+
 	public static void dl(String tag, String message) {
 		LOG.debug(tag + TAG_MESSAGE_SEPARATOR + message);
 	}
 
 	public static void dl(String message) {
 		LOG.debug(DEBUG_LIVE_TAG + TAG_MESSAGE_SEPARATOR + message);
+	}
+
+	public static void dl(String tag, Throwable throwable) {
+		LOG.debug(tag, throwable);
 	}
 }
