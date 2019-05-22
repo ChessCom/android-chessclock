e55e49ec7bd1bcc5d285a65b9c008b01d04e7833
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2019-05-23 01:33:14 +0430

    Migrate to AndroidX

--- app/src/main/java/com/chess/clock/service/ChessClockLocalService.java
+++ app/src/main/java/com/chess/clock/service/ChessClockLocalService.java
@@ -8,7 +8,7 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.PowerManager;
-import android.support.v4.app.NotificationCompat;
+import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
 import com.chess.clock.R;
