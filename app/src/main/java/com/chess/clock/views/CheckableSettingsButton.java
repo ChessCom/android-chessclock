e43fcbec9f1505040d1a586ee2b571e35cd94cf5
Michal Piekarski <michal@chess.com>
2022-09-14 12:49:50 +0200

    layout

---
+++ app/src/main/java/com/chess/clock/views/CheckableSettingsButton.java
@@ -0,0 +1,22 @@
+package com.chess.clock.views;
+
+import android.widget.Checkable;
+
+public class CheckableSettingsButton implements Checkable {
+
+
+    @Override
+    public void setChecked(boolean checked) {
+
+    }
+
+    @Override
+    public boolean isChecked() {
+        return false;
+    }
+
+    @Override
+    public void toggle() {
+
+    }
+}
