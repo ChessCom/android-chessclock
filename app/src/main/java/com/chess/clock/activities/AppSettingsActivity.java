a9a467fe271ab8439c1008e56260594567020926
Michal Piekarski <michal@chess.com>
2022-09-14 11:07:14 +0200

    created activity

---
+++ app/src/main/java/com/chess/clock/activities/AppSettingsActivity.java
@@ -0,0 +1,14 @@
+package com.chess.clock.activities;
+
+import android.os.Bundle;
+
+import com.chess.clock.R;
+
+public class AppSettingsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_settings);
+    }
+}
