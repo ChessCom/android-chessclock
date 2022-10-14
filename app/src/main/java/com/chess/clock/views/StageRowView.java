08b1c269fdaecf590b5f557cc5b555d4d20fc3ed
Michal Piekarski <michal@chess.com>
2022-10-14 14:01:02 +0200

    stage edit updates

--- app/src/main/java/com/chess/clock/views/StageRowView.java
+++ app/src/main/java/com/chess/clock/views/StageRowView.java
@@ -31,7 +31,7 @@ public class StageRowView extends ConstraintLayout {
         timeIncrementDetails = view.findViewById(R.id.incrementDetailsTv);
     }
 
-    public void updateData(int position, Stage stage, TimeIncrement timeIncrement) {
+    public void updateData(int position, Stage stage) {
         positionLabel.setText(String.valueOf(position));
         String details = "";
         String timeFormatted = stage.durationTimeFormatted();
@@ -41,6 +41,6 @@ public class StageRowView extends ConstraintLayout {
             details = getResources().getQuantityString(R.plurals.x_moves_in, stage.getTotalMoves(), stage.getTotalMoves(), timeFormatted);
         }
         stageDetails.setText(details);
-        timeIncrementDetails.setText(timeIncrement.toString());
+        timeIncrementDetails.setText(stage.getTimeIncrement().toString());
     }
 }
