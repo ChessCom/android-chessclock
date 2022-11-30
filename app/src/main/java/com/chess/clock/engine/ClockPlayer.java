627c5c35b6b083cf31b106b2d74ec20436741c90
Michał Piekarski <michal.piekarski@byoutline.com>
2022-11-30 13:55:23 +0100

    moves count fix for new simple time control (#59)

--- app/src/main/java/com/chess/clock/engine/ClockPlayer.java
+++ app/src/main/java/com/chess/clock/engine/ClockPlayer.java
@@ -3,6 +3,10 @@ package com.chess.clock.engine;
 public enum ClockPlayer {
     ONE, TWO;
 
+    public boolean isFirstPlayer() {
+        return this == ONE;
+    }
+
     public static ClockPlayer ofBoolean(boolean playerOne) {
         return playerOne ? ONE : TWO;
     }
